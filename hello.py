2e50ea74b565ea5860a477a2c3b21cd4945814b3
AndreaVee4 <s3844969@students.fscj.edu>
2025-08-04 19:27:49 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,11 @@
 # 08/04/2025
 # A simple Python progam to print a message
 
+# Andrea Villarreal
+# 08/04/2025
+# added a new message
+
 print("Hello, World!")
+
+print("It's a thunderstormin' day!")
+
